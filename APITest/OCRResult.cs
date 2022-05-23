9c6df26cfc1d9daf1542ab2be8cd669dd89f0c78
Derek Antrican <derekantrican@gmail.com>
2022-05-23 11:17:39 -0700

    Fixed type and proper CamelCase

--- APITest/OCRResult.cs
+++ APITest/OCRResult.cs
@@ -1,20 +1,20 @@
 ﻿namespace OCRAPITest
 {
 
-    public class Rootobject
+    public class RootObject
     {
-        public Parsedresult[] ParsedResults { get; set; }
+        public ParsedResult[] ParsedResults { get; set; }
         public int OCRExitCode { get; set; }
         public bool IsErroredOnProcessing { get; set; }
         public string ErrorMessage { get; set; }
         public string ErrorDetails { get; set; }
     }
 
-    public class Parsedresult
+    public class ParsedResult
     {
         public object FileParseExitCode { get; set; }
         public string ParsedText { get; set; }
-        public string ErrorMessage { get; set; }
+        public string[] ErrorMessage { get; set; }
         public string ErrorDetails { get; set; }
     }
 
